09f9c9bc320664ebb9ced1098338825f99c8db3c
github username <github email address>
2017-11-07 17:46:10 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,12 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resource :authors, only: [:create]
+  get '/authors/new', to: 'authors#new', as: 'new_author'
+  get '/author/:id', to: 'authors#show', as: 'author'
+  get '/author/:id/edit', to: 'authors#edit', as: 'edit_author'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resource :posts, only: [:create]
+  get '/post/new', to: 'posts#new', as: 'new_post'
+  get '/post/:id', to: 'posts#show', as: 'post'
+  get '/post/:id/edit', to: 'posts#edit', as: 'edit_post'
+  patch '/post/:id', to: 'posts#update'
 end
